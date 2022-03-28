878525ff59158d6ed86c24f2fda18c5b2932d20d
jfrance00 <jfrance00@gmail.com>
2022-03-28 14:11:45 +0300

    xpNinja done

--- Classes/week3/Day2/xpNinja.js
+++ Classes/week3/Day2/xpNinja.js
@@ -21,3 +21,37 @@ let numberArray = userNumbers.split(",").map(Number)
 const sum = numberArray.reduce((partialSum, a) => partialSum + a, 0);
 
 console.log(sum);
+
+// Exercise 3
+
+let userPhrase = prompt("Write a sentance with 'Nemo' in it");
+
+let indexNemo = userPhrase.indexOf("Nemo");
+
+if (indexNemo == -1) {
+    alert("'Nemo' is not in your phrase");
+} else {
+    alert(`'Nemo' is found at index ${indexNemo}`);
+}
+
+// Exercise 4
+
+let userNumber = prompt("please enter a number");
+
+if (userNumber <= 2) {
+    alert("boom");
+} else {
+    let returnMessage = "b"; 
+    for (let i = 0; i < userNumber; i++){
+        returnMessage = returnMessage + "o"
+    } 
+    returnMessage = returnMessage + "m";
+
+    if (userNumber % 2 == 0){
+        returnMessage = returnMessage + "!";
+    }
+    if (userNumber % 5 == 0){
+        returnMessage = returnMessage.toUpperCase();
+    }
+    alert(returnMessage)
+}
